9f9195f853ad1166baed28e138fab0e8277635d5
Faizan_Ali <faizanperficient@gmail.com>
2022-01-12 18:46:01 +0530

    changes done in the Assignment on String and its method.

--- Faizan-String_its method/stringmethod.js
+++ Faizan-String_its method/stringmethod.js
@@ -1,42 +1,27 @@
-function PersonData() {
-    //store input value in varibles
-    var fname = document.getElementById("fname").value;
-    var lname = document.getElementById("lname").value;
+
+function persondata() {
+   
+    var first_name = document.getElementById("fname").value;
+    var last_name = document.getElementById("lname").value;
     var age = document.getElementById("age").value;
     var email = document.getElementById("email").value;
-    //convert the first name into title case
-    var fname = fname.toLowerCase().split(' ').map(function (word) {
-        return (word.charAt(0).toUpperCase() + word.slice(1));
-    }).join(' ');
-    //convert the last name into title case
-    var lname = lname.toLowerCase().split(' ').map(function (word1) {
-        return (word1.charAt(0).toUpperCase() + word1.slice(1));
-    }).join(' ');
-
-    //check all input have some values or not
-    if (age == "" || fname == "" || lname == "" || email == "") {
-        alert("Please input a Value");
-        return false;
+   function firstname(){
+        var first_name2 = first_name.charAt(0).toUpperCase()+first_name.slice(1).toLowerCase();;
+        return first_name2;
     }
-    //check the age is satisfy for condition
+    function lastname(){
+        var last_name2 = last_name.charAt(0).toUpperCase()+last_name.slice(1).toLowerCase();;
+        return last_name2;
+    }
     if (age >= 10) {
-        document.getElementById("pi3").innerHTML = "your age is " + age + " thats why u are Genious";
+        document.getElementById("pi3").innerHTML = "Your Age is " + age + " and You are Genious";
     } else if (age <= 0) {
-        alert("please enter valid value in age")
-    } else if (age < 10) {
-        document.getElementById("pi3").innerHTML = "your age is " + age + " thats why u are Kids";
+        alert("please enter valid value in age");
+        return false;
+    } else if (age < 10 ) {
+        document.getElementById("pi3").innerHTML = "Your Age is " + age + " and You are Kid";
     }
-    //print the all input value into paragraph
-    document.getElementById("pi").innerHTML = "Your first name is :-" + fname;
-    document.getElementById("pi2").innerHTML = "Your last name is:-" + lname;
+    document.getElementById("pi").innerHTML = "Your first name is :-" + firstname();
+    document.getElementById("pi2").innerHTML = "Your last name is:-" + lastname();
     document.getElementById("pi4").innerHTML = "Your Email id  is:-" + email;
-
-}
-//function to delete all text into input box
-function Clearall() {
-    document.getElementById("fname").value = "";
-    document.getElementById("lname").value = "";
-    document.getElementById("age").value = "";
-    document.getElementById("email").value = "";
-    
 }
